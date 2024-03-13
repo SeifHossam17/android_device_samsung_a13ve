dd81b1da7a63b0dcd0a3eb02bdcadf1e87bd4ecc
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-03-13 19:43:52 +0200

    Update and rename omni_a13ve.mk to twrp_a13ve.mk

--- twrp_a13ve.mk
+++ twrp_a13ve.mk
@@ -6,16 +6,16 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from twrp product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a13ve device
 $(call inherit-product, device/samsung/a13ve/device.mk)
 
 PRODUCT_DEVICE := a13ve
-PRODUCT_NAME := omni_a13ve
+PRODUCT_NAME := twrp_a13ve
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A137F
 PRODUCT_MANUFACTURER := samsung
