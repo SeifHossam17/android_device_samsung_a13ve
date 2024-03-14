02302e999cbf596076bc7b77ae83edd2fa2a8495
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-03-14 20:23:00 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,4 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/samsung/a13ve
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mtk \
+    fastbootd
